fcb7f1b1f98f2f1d4a46dad5546d2e9b715b0eac
nikita3006 <nikitanegi3011@gmail.com>
2023-12-16 21:54:15 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ExpenseItems from './component/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+export default App;
---
+++ src/component/ExpenseItem.jsx
@@ -0,0 +1,12 @@
+function ExpenseItems() {
+    return (
+      <div>
+        <h3>ExpenseItems</h3>
+        <h2>ExpenseItems</h2>
+        <p>Food Rs.10</p>
+        <p>Petrol Rs.100</p>
+        <p>Movies Rs.200</p>
+      </div>
+    );
+  }
+  export default ExpenseItems;
