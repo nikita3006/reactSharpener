b64114251f8eb8c40cbbcb114c630bb5d76b02ff
nikita3006 <nikitanegi3011@gmail.com>
2023-12-18 19:59:19 +0530

    styleDebug

--- src/component/ExpenseItem.jsx
+++ src/component/ExpenseItem.jsx
@@ -1,12 +1,19 @@
-function ExpenseItems() {
-    return (
-      <div>
-        <h3>ExpenseItems</h3>
-        <h2>ExpenseItems</h2>
-        <p>Food Rs.10</p>
-        <p>Petrol Rs.100</p>
-        <p>Movies Rs.200</p>
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 4, 17);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const LocationOfExpenditure = "Pune";
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
-    );
-  }
-  export default ExpenseItems;
+      </div>
+  );
+}
+export default ExpenseItem;
