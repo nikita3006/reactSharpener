040b0c2ef8966224fc3ab98b300e890a549a7fc9
nikita3006 <nikitanegi3011@gmail.com>
2023-12-23 23:55:31 +0530

    Manipulating the DOM - React States

--- src/ExpenseList/ExpenseItem.jsx
+++ src/ExpenseList/ExpenseItem.jsx
@@ -1,32 +1,42 @@
-import "./ExpenseItem.css";
-
+import "./ExpenseItems.css";
 import ExpenseDate from "./ExpenseDate";
-
 import ExpenseDetails from "./ExpenseDetails";
-
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const ExpenseItems = (props) => {
-  const deleteButton = (e) => {
-    let item = e.target.parentElement;
-    item.remove();
-  };
 
+  const deleteButton = (e) => {
+
+    let item = e.target.parentElement;
+    item.remove() 
+
+  }
+
+  const [name, setTitle] = useState(props.name)
+  const [amount , setAmount] = useState(props.amount)
+
+  const editButton = (e) =>{
+    setTitle("Updated!")
+    setAmount("100$")
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
-
       <div className="expense-item__description">
-        <h2>{props.name}</h2>
-
+        <h2>{name}</h2>
         <ExpenseDetails
+          
           LocationOfExpenditure={props.LocationOfExpenditure}
-          amount={props.amount}
-        ></ExpenseDetails>
+          amount={amount}
+        >   
+        </ExpenseDetails>
       </div>
-      <button onClick={deleteButton}>Delete Expense</button>
-     
+
+      <button onClick={deleteButton} >Delete Expense</button>
+      <button onClick={editButton} > Edit Expense</button>
     </Card>
   );
-};
+}
+
 export default ExpenseItems;
